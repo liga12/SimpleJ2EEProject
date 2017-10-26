43ee23cad7bc725853f4ad1342333274052df1a7
Sas <123@mail.ru>
2017-10-26 16:07:33 +0300

    Create HttpServlet

---
+++ src/main/java/com/teaminternational/web/MyHttpServlet.java
@@ -0,0 +1,16 @@
+package com.teaminternational.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet("/")
+public class MyHttpServlet extends BaseHttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
