3cf55222f233898ec101ee0ea27e7e54bd868321
Sas <123@mail.ru>
2017-10-26 16:24:32 +0300

    Get instance of HelloWorldGenerator from context BaseHttpServlet.getBean() and print it in page (use htm page structure)

--- src/main/java/com/teaminternational/web/MyHttpServlet.java
+++ src/main/java/com/teaminternational/web/MyHttpServlet.java
@@ -1,16 +1,22 @@
 package com.teaminternational.web;
 
+import com.teaminternational.HelloWorldGenerator;
+
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-
-@WebServlet("/")
 public class MyHttpServlet extends BaseHttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doGet(req, resp);
+        HelloWorldGenerator generator = getBean(HelloWorldGenerator.COMPONENT_NAME);
+
+        resp.setContentType("text/html");
+
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>" + generator + "</h1>");
     }
 }
