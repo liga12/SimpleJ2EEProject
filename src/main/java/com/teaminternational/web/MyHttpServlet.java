edff709fdbafaae44c0d4a1a94e6364e8ea87d9f
Sas <123@mail.ru>
2017-10-26 16:38:48 +0300

    refactoring

--- src/main/java/com/teaminternational/web/MyHttpServlet.java
+++ src/main/java/com/teaminternational/web/MyHttpServlet.java
@@ -13,10 +13,10 @@ public class MyHttpServlet extends BaseHttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doGet(req, resp);
         HelloWorldGenerator generator = getBean(HelloWorldGenerator.COMPONENT_NAME);
-
+        generator.sayHello();
         resp.setContentType("text/html");
 
         PrintWriter out = resp.getWriter();
-        out.println("<h1>" + generator + "</h1>");
+        out.println("<h1>" + generator.sayHello() + "</h1>");
     }
 }
