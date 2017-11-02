beb53f743feea43964fe8c824969446c5ed678ba
vzubencko <vladimir.zubencko@gmail.com>
2017-11-02 10:17:47 +0200

    fix bug

--- src/main/java/com/teaminternational/web/MyHttpServlet.java
+++ src/main/java/com/teaminternational/web/MyHttpServlet.java
@@ -11,11 +11,9 @@ import java.io.PrintWriter;
 public class MyHttpServlet extends BaseHttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
         HelloWorldGenerator generator = getBean(HelloWorldGenerator.COMPONENT_NAME);
         generator.sayHello();
         resp.setContentType("text/html");
-
         PrintWriter out = resp.getWriter();
         out.println("<h1>" + generator.sayHello() + "</h1>");
     }
